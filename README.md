e621fe773e850333cfd8394f92317fb8ddacdcc1
egawata <egawa.takashi@gmail.com>
2019-01-19 10:49:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Teng::Plugin::TmpSuppressRowObjects - It's new $module
+
+# SYNOPSIS
+
+    use Teng::Plugin::TmpSuppressRowObjects;
+
+# DESCRIPTION
+
+Teng::Plugin::TmpSuppressRowObjects is ...
+
+# LICENSE
+
+Copyright (C) egawata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+egawata <egawa.takashi@gmail.com>
---
+++ lib/Teng/Plugin/TmpSuppressRowObjects.pm
@@ -0,0 +1,39 @@
+package Teng::Plugin::TmpSuppressRowObjects;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Teng::Plugin::TmpSuppressRowObjects - It's new $module
+
+=head1 SYNOPSIS
+
+    use Teng::Plugin::TmpSuppressRowObjects;
+
+=head1 DESCRIPTION
+
+Teng::Plugin::TmpSuppressRowObjects is ...
+
+=head1 LICENSE
+
+Copyright (C) egawata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+egawata E<lt>egawa.takashi@gmail.comE<gt>
+
+=cut
+
